74290f0e95c659235ef78bbee43345085224d7ad
KingDole <brandon.bartee1989@gmail.com>
2025-09-28 22:38:02 -0400

    Navbar
    Created a react project and created a Navbar for the website.

---
+++ react-project/src/pages/CourseCatalog.jsx
@@ -0,0 +1,3 @@
+export default function CourseCatalog(){
+    return <h1>This is a list of the currently available courses with descriptions.</h1>
+}
---
+++ react-project/src/pages/CreateDelete.jsx
@@ -0,0 +1,3 @@
+export default function CreateDelete(){
+    return <h1>This is where teachers can create or delete courses.</h1>
+}
